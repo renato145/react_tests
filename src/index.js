1c11d9b3a5f3e76ad691662161a2623b7c59650c
Renato <renato145@hotmail.com>
2020-01-03 13:39:06 +1030

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// import App from './App';
+
+const App = () => {
+  const [ x, setX ] = useState(0);
+  const changeNumber = () => setTimeout(() => {
+    setX(Math.random());
+  }, 200);
+  useEffect(() => { changeNumber() } );
+  return <span>{x}</span>;
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
