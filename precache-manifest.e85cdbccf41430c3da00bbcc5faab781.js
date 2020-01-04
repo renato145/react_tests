1b0f768d57198f193ab44e0a7e2dca2441b728f3
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-01-04 21:13:14 +1030

    Updates

---
+++ precache-manifest.e85cdbccf41430c3da00bbcc5faab781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89712a2d4627d2bd5b3a1e652b524b10",
+    "url": "/react_tests/index.html"
+  },
+  {
+    "revision": "850e651060868e81d668",
+    "url": "/react_tests/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3c5bfc18c798effbc833",
+    "url": "/react_tests/static/js/2.0095e62a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_tests/static/js/2.0095e62a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "850e651060868e81d668",
+    "url": "/react_tests/static/js/main.6755133e.chunk.js"
+  },
+  {
+    "revision": "d4feb29d300dba1a0aee",
+    "url": "/react_tests/static/js/runtime-main.fff134aa.js"
+  },
+  {
+    "revision": "52f45d7da2310a45923fd520a15c174b",
+    "url": "/react_tests/static/media/skin_cancer_au.52f45d7d.csv"
+  }
+]);
