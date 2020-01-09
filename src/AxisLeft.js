b38e1475ace0de9d6cf415bed1d348636bcd6992
Renato <renato145@hotmail.com>
2020-01-09 16:08:11 +1030

    test

--- src/AxisLeft.js
+++ src/AxisLeft.js
@@ -1,4 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+const d3 = require('d3');
+
+// export const AxisLeft = ({ yScale, innerWidth, tickOffset = 3 }) => {
+//   const g = useRef(null);
+//   useEffect(() => {
+//     // const yAxis = d3.svg.axis().scale(yScale).orient('left');
+//     const axis = d3.select(g.current).append('g')
+//       .transition().duration(200)
+//       .call(d3.axisLeft(yScale));
+//       // .attr('font-size', '1em');
+//     axis.selectAll('.domain, .tick line').remove();;
+//   }, [yScale]);
+
+//   return (<g ref={g}>
+//   </g>);
+// };
 
 export const AxisLeft = ({ yScale, innerWidth, tickOffset = 3 }) =>
   yScale.ticks().map((tickValue, i) => (
